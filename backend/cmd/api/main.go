11693b967d5d06321084c9a782783a507c7c30a3
Adrian DY <adrian.dy89@gmail.com>
2025-01-27 21:42:09 +0100

    init repo

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = 8080
+
+type application struct {
+	Domain string
+}
+
+func main() {
+	// set application configuration
+	var app application
+
+	// read from command line arguments
+
+	// connect to database
+
+	app.Domain = "http://localhost:8080"
+	log.Printf("Starting server on %s", app.Domain)
+
+	// start web server
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Println("server failed to start")
+		log.Fatal(err)
+	}
+}
