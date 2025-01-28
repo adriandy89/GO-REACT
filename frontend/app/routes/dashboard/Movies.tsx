b12bf42551ef0882b4081469b123154d89f08d82
Adrian DY <adrian.dy89@gmail.com>
2025-01-28 11:53:07 +0100

    cors

--- frontend/app/routes/dashboard/Movies.tsx
+++ frontend/app/routes/dashboard/Movies.tsx
@@ -14,26 +14,31 @@ const MoviesPage = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
-    let moviesList: Movie[] = [
-      {
-        id: 1,
-        title: "Highlander",
-        release_date: "1986-03-07",
-        runtime: 116,
-        mpaa_rating: "R",
-        description: "Some long description",
-      },
-      {
-        id: 2,
-        title: "Raiders of the Lost Ark",
-        release_date: "1981-06-12",
-        runtime: 115,
-        mpaa_rating: "PG-13",
-        description: "Some long description",
-      },
-    ];
+    // let moviesList: Movie[] = [
+    //   {
+    //     id: 1,
+    //     title: "Highlander",
+    //     release_date: "1986-03-07",
+    //     runtime: 116,
+    //     mpaa_rating: "R",
+    //     description: "Some long description",
+    //   },
+    //   {
+    //     id: 2,
+    //     title: "Raiders of the Lost Ark",
+    //     release_date: "1981-06-12",
+    //     runtime: 115,
+    //     mpaa_rating: "PG-13",
+    //     description: "Some long description",
+    //   },
+    // ];
+    fetch("http://localhost:8080/movies").then((response) => {
+      response.json().then((data) => {
+        setMovies(data);
+      });
+    });
 
-    setMovies(moviesList);
+    // setMovies(moviesList);
   }, []);
 
   return (
